6b665cdd44236f3650c308b4b05dd24fcf7eb869
prabalsh19 <40392568+prabalsh19@users.noreply.github.com>
2023-07-22 21:40:51 +0530

    Routes - Update and delete tasks

--- controllers/task.js
+++ controllers/task.js
@@ -19,3 +19,25 @@ export const createTaskHandler = (req, res) => {
   });
   res.json({ success: true, message: "Task created" });
 };
+
+export const deletePostHandler = async (req, res) => {
+  const { id } = req.params;
+
+  await Task.findByIdAndDelete({ _id: id }, { new: true });
+
+  res.json({
+    success: true,
+    post,
+    message: "post deleted",
+  });
+};
+
+export const editTaskHandler = async (req, res) => {
+  const { id } = req.params;
+  const { task } = req.body;
+
+  const posts = await Task.findByIdAndUpdate(id, { task });
+  res.json({
+    posts,
+  });
+};
--- router/list.js
+++ router/list.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getAllTaskHandler, createTaskHandler } from "../controllers/task.js";
+import {
+  getAllTaskHandler,
+  createTaskHandler,
+  deletePostHandler,
+  editTaskHandler,
+} from "../controllers/task.js";
 import { isAuthenticated } from "../middleware/auth.js";
 const router = express.Router();
 
 router.get("/tasks", isAuthenticated, getAllTaskHandler);
-
 router.post("/task/add", isAuthenticated, createTaskHandler);
-
+router.delete("/task/remove/:id", deletePostHandler);
+router.put("/task/edit/:id", editTaskHandler);
 export default router;
